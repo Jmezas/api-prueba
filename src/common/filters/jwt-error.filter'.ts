7696ccf85600f40bdaefaee8f8cb12ab7dd6c706
Jmezas <yaser77334600@gmail.com>
2023-01-26 20:12:27 -0500

    confing

--- src/common/filters/jwt-error.filter'.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-
-export class JwtErrorFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {
-    const response = host.switchToHttp().getResponse();
-    response.status(401).json({ message: 'Invalid token' });
-  }
-}
